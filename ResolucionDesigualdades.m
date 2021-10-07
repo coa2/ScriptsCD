cabf645b1eb81729dd01ce716faf8b5ce1ff2efa
Marmolejo García Héctor Daniel <91081737+HectorDanielMarmolejoGarcia@users.noreply.github.com>
2021-10-07 18:10:01 -0500

    Resolución de desigualdades
    Código completo

---
+++ ResolucionDesigualdades.m
@@ -0,0 +1,43 @@
+% Octave Script 
+% Title          :Resolucion de desigualdades 
+% Descripcion    :Script para resolver inecuaciones desde octave
+% Author         :Hector Daniel Marmoleo Garcia 
+%                :Camila Cruz Jimenez
+%                :David Garcia Rodriguez 
+% Date           :20211006
+% Version        :1
+% Usage          :octave> cd /path/
+%                :octave> ResolucionDesigualdades
+% Notes          :Requiere aplicacion octave
+%   
+syms x
+disp('Solucion a la inecuacion numero 1 "Incecuacion lineal con una incognita"');
+x + 2 <= 0
+solve( x + 2 <= 0)
+
+disp('Solucion a la inecuacion numero 2 "Incecuacion cuadratica con una incognita"');
+x^2 + 3 >= 12
+solve(x^2 + 3 >= 12)
+
+disp('Solucion a la inecuacion numero 3 "Incecuacion con un valor absoluto"');
+abs(x) < 5
+solve(abs(x) < 5)
+
+
+hold on
+plot([-20 5],[0,0],'k+-',"linewidth",3,"markersize",12)
+plot([0 0],[-20 30], 'k-',"linewidth",3)
+
+x = -20:0.01:11;
+y = x + 2;
+plot(x,y,"linewidth",2)
+
+x = -100:1:100;
+f = 'x.^2 + 3';
+fplot(f,"linewidth",2);
+
+x = -100:1:100;
+y2 = abs(x) < 5;
+plot(x,y2,"linewidth",2)
+
+hold off
